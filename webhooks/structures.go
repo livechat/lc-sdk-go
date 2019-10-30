a363f873c0a7f5fe26e95eff5cbff5d544f67d93
Krystian Ogierman <k.ogierman@livechatinc.com>
2019-10-30 16:13:55 +0100

    Fix handling of User type in chat_user_added webhook

--- webhooks/structures.go
+++ webhooks/structures.go
@@ -2,7 +2,7 @@ package webhooks
 
 import (
 	"encoding/json"
-
+	"time"
 	"github.com/livechat/lc-sdk-go/customer"
 	"github.com/livechat/lc-sdk-go/objects/events"
 )
@@ -32,11 +32,85 @@ type AccessSet struct {
 	Access   customer.Access `json:"access"`
 }
 
+type User struct {
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	Avatar   string `json:"avatar"`
+	Email    string `json:"email"`
+	Present  bool   `json:"present"`
+	LastSeen customer.Time   `json:"last_seen_timestamp"`
+	userSpecific
+}
+
+type userSpecific struct {
+	RoutingStatus json.RawMessage `json:"routing_status"`
+	LastVisit  json.RawMessage `json:"last_visit"`
+	Statistics json.RawMessage `json:"statistics"`
+	AgentLastEventCreatedAt    json.RawMessage `json:"agent_last_event_created_at"`
+	CustomerLastEventCreatedAt json.RawMessage `json:"customer_last_event_created_at"`
+}
+
+type Agent struct {
+	RoutingStatus string `json:"routing_status"`
+	*User
+}
+
+type Customer struct {
+	LastVisit  customer.Visit `json:"last_visit"`
+	Statistics struct {
+		VisitsCount            int `json:"visits_count"`
+		ThreadsCount           int `json:"threads_count"`
+		ChatsCount             int `json:"chats_count"`
+		PageViewsCount         int `json:"page_views_count"`
+		GreetingsShownCount    int `json:"greetings_shown_count"`
+		GreetingsAcceptedCount int `json:"greetings_accepted_count"`
+	}
+	AgentLastEventCreatedAt    time.Time `json:"agent_last_event_created_at"`
+	CustomerLastEventCreatedAt time.Time `json:"customer_last_event_created_at"`
+	*User
+}
+
+func (u *User) Agent() *Agent {
+	if u.Type != "agent" {
+		return nil
+	}
+	var a Agent
+
+	a.User = u
+	if err := json.Unmarshal(u.RoutingStatus, &a.RoutingStatus); err != nil {
+		return nil
+	}
+	return &a
+}
+
+func (u *User) Customer() *Customer {
+	if u.Type != "customer" {
+		return nil
+	}
+	var c Customer
+
+	c.User = u
+	if err := json.Unmarshal(u.LastVisit, &c.LastVisit); err != nil {
+		return nil
+	}
+	if err := json.Unmarshal(u.Statistics, &c.Statistics); err != nil {
+		return nil
+	}
+	if err := json.Unmarshal(u.AgentLastEventCreatedAt, &c.AgentLastEventCreatedAt); err != nil {
+		return nil
+	}
+	if err := json.Unmarshal(u.CustomerLastEventCreatedAt, &c.CustomerLastEventCreatedAt); err != nil {
+		return nil
+	}
+	return &c
+}
+
 type ChatUserAdded struct {
-	ChatID   string        `json:"chat_id"`
-	ThreadID string        `json:"thread_id"`
-	User     customer.User `json:"user"`
-	UserType string        `json:"user_type"` // TODO shall this be enum?
+	ChatID   string  `json:"chat_id"`
+	ThreadID string  `json:"thread_id"`
+	User     User    `json:"user"`
+	UserType string  `json:"user_type"` // TODO shall this be enum?
 }
 
 type ChatUserRemoved struct {
--- webhooks/tests/validators/chat_user_added_validator.go
+++ webhooks/tests/validators/chat_user_added_validator.go
@@ -17,17 +17,45 @@ func ChatUserAdded(licenseID int, payload interface{}) error {
 
 	var errors string
 	PropEq("ChatID", wh.ChatID, "PJ0MRSHTDG", &errors)
-	PropEq("UserType", wh.UserType, "agent", &errors)
+	PropEq("UserType", wh.UserType, "customer", &errors)
 
-	user := wh.User
-	PropEq("User.ID", user.ID, "l.wojciechowski@livechatinc.com", &errors)
-	PropEq("User.Type", user.Type, "agent", &errors)
-	PropEq("User.Name", user.Name, "Łukasz Wojciechowski", &errors)
-	PropEq("User.Email", user.Email, "l.wojciechowski@livechatinc.com", &errors)
-	PropEq("User.Avatar", user.Avatar, "livechat.s3.amazonaws.com/default/avatars/a14.png", &errors)
-	PropEq("User.Present", user.Present, true, &errors)
-	PropEq("User.LastSeen", user.LastSeen.String(), "1970-01-01 01:00:00 +0100 CET", &errors)
-	//	PropEq("User.RoutingStatus", user.RoutingStatus, "accepting_chats", &errors) ERROR - Incorrect type User instead of Customer or Agent, some data might be missing
+	customer := wh.User.Customer()
+	PropEq("Customer.ID", customer.ID, "345f8235-d60d-433e-63c5-7f813a6ffe25", &errors)
+	PropEq("Customer.Type", customer.Type, "customer", &errors)
+	PropEq("Customer.Name", customer.Name, "test", &errors)
+	PropEq("Customer.Email", customer.Email, "test@test.pl", &errors)
+	PropEq("Customer.Avatar", customer.Avatar, "", &errors)
+	PropEq("Customer.Present", customer.Present, true, &errors)
+	PropEq("Customer.LastSeen", customer.LastSeen.String(), "2019-10-08 13:56:53 +0200 CEST", &errors)
+
+	lastVisit := customer.LastVisit
+	PropEq("LastVisit.IP", lastVisit.IP, "37.248.156.62", &errors)
+	PropEq("LastVisit.UserAgent", lastVisit.UserAgent, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36", &errors)
+	PropEq("LastVisit.StartedAt", lastVisit.StartedAt.String(), "2019-10-11 09:40:56.071345 +0000 UTC", &errors)
+
+	geolocation := lastVisit.Geolocation
+	PropEq("Geolocation.Country", geolocation.Country, "Poland", &errors)
+	PropEq("Geolocation.CountryCode", geolocation.CountryCode, "PL", &errors)
+	PropEq("Geolocation.Region", geolocation.Region, "test", &errors)
+	PropEq("Geolocation.City", geolocation.City, "Wroclaw", &errors)
+	PropEq("Geolocation.Timezone", geolocation.Timezone, "test_timezone", &errors)
+
+	PropEq("LastPages", len(lastVisit.LastPages), 1, &errors)
+
+	PropEq("LastPages.OpenedAt", lastVisit.LastPages[0].OpenedAt.String(), "2019-10-11 09:40:56.071345 +0000 UTC", &errors)
+	PropEq("LastPages.URL", lastVisit.LastPages[0].URL, "https://cdn.livechatinc.com/labs/?license=100007977/", &errors)
+	PropEq("LastPages.Title", lastVisit.LastPages[0].Title, "LiveChat", &errors)
+
+	statistics := customer.Statistics
+	PropEq("Statistics.VisistsCount", statistics.VisitsCount, 29, &errors)
+	PropEq("Statistics.ThreadsCount", statistics.ThreadsCount, 18, &errors)
+	PropEq("Statistics.ChatsCount", statistics.ChatsCount, 1, &errors)
+	PropEq("Statistics.PageViewsCount", statistics.PageViewsCount, 5, &errors)
+	PropEq("Statistics.GreetingsShownCount", statistics.GreetingsShownCount, 6, &errors)
+	PropEq("Statistics.GreetingsAcceptedCount", statistics.GreetingsAcceptedCount, 8, &errors)
+
+	PropEq("Customer.AgentLastEventCreatedAt", customer.AgentLastEventCreatedAt.String(), "2019-10-11 09:40:59.249 +0000 UTC", &errors)
+	PropEq("Customer.CustomerLastEventCreatedAt", customer.CustomerLastEventCreatedAt.String(), "2019-10-11 09:40:59.219001 +0000 UTC", &errors)
 
 	if errors != "" {
 		return fmt.Errorf(errors)
