1fac054ccf73eaf019bf4a5ebbfccbd96f4b8030
Krystian Ogierman <k.ogierman@livechatinc.com>
2019-11-25 12:34:52 +0100

    Selectively 'merge' changes from feature/configuration-api branch

--- configuration/api.go
+++ configuration/api.go
@@ -1,35 +1,21 @@
 package configuration
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"time"
 
-	api_errors "github.com/livechat/lc-sdk-go/errors"
+	"github.com/livechat/lc-sdk-go/internal"
+	"github.com/livechat/lc-sdk-go/objects/authorization"
 )
 
-const apiVersion = "v3.2"
+const apiVersion = "v3.1"
 
 type API struct {
-	httpClient  *http.Client
-	ApiURL      string
-	clientID    string
-	tokenGetter func() *Token
+	base internal.APIBase
 }
 
-type Token struct {
-	LicenseID   int
-	AccessToken string
-	Region      string
-}
-
-type TokenGetter func() *Token
-
-func NewAPI(t TokenGetter, client *http.Client, clientID string) (*API, error) {
+func NewAPI(t authorization.TokenGetter, client *http.Client, clientID string) (*API, error) {
 	if t == nil {
 		return nil, errors.New("cannot initialize api without TokenGetter")
 	}
@@ -41,80 +27,41 @@ func NewAPI(t TokenGetter, client *http.Client, clientID string) (*API, error) {
 	}
 
 	return &API{
-		tokenGetter: t,
-		ApiURL:      "https://api.livechatinc.com/",
-		clientID:    clientID,
-		httpClient:  client,
+		internal.APIBase{
+			ApiVersion:  apiVersion,
+			ApiName:     "configuration",
+			TokenGetter: t,
+			ApiURL:      "https://api.livechatinc.com",
+			ClientID:    clientID,
+			HttpClient:  client,
+		},
 	}, nil
 }
 
-func (a *API) RegisterWebhook(wh *Webhook) (string, error) {
-	var resp registerWebhookResponse
-	err := a.call("register_webhook", wh, &resp)
+func (a *API) RegisterWebhook(w *Webhook) (string, error) {
+	var resp struct {
+		WebhookID string `json:"webhook_id"`
+	}
+	err := a.base.Call("register_webhook", w, &resp)
 	if err != nil {
 		return "", err
 	}
-	return resp.ID, nil
+	return resp.WebhookID, err
+}
+
+func (a *API) GetWebhooksConfig() ([]WebhookInfo, error) {
+	req := map[string]string{}
+	ws := make([]WebhookInfo, 0)
+	err := a.base.Call("get_webhooks_config", req, &ws)
+
+	return ws, err
 }
 
 func (a *API) UnregisterWebhook(id string) error {
-	req := &unregisterWebhookRequest{
-		ID: id,
-	}
-	return a.call("unregister_webhook", req, nil)
+	req := map[string]string{"webhook_id": id}
+	return a.base.Call("unregister_webhook", req, nil)
 }
 
-func (a *API) GetWebhooksConfig() ([]*RegisteredWebhook, error) {
-	resp := []*RegisteredWebhook{}
-	err := a.call("get_webhooks_config", nil, &resp)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-func (a *API) call(action string, reqPayload interface{}, respPayload interface{}) error {
-	rawBody, err := json.Marshal(reqPayload)
-	if err != nil {
-		return err
-	}
-	token := a.tokenGetter()
-	if token == nil {
-		return fmt.Errorf("couldn't get token")
-	}
-
-	url := fmt.Sprintf("%s/%s/configuration/action/%s", a.ApiURL, apiVersion, action)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(rawBody))
-	if err != nil {
-		return err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
-	req.Header.Set("User-agent", fmt.Sprintf("GO SDK Application %s", a.clientID))
-	req.Header.Set("X-Region", token.Region)
-
-	resp, err := a.httpClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-
-	if resp.StatusCode != http.StatusOK {
-		apiErr := &api_errors.ErrAPI{}
-		if err := json.Unmarshal(bodyBytes, apiErr); err != nil {
-			return fmt.Errorf("couldn't unmarshal error response: %s (code: %d, raw body: %s)", err.Error(), resp.StatusCode, string(bodyBytes))
-		}
-		if apiErr.Error() == "" {
-			return fmt.Errorf("couldn't unmarshal error response (code: %d, raw body: %s)", resp.StatusCode, string(bodyBytes))
-		}
-		return apiErr
-	}
-
-	if err != nil {
-		return err
-	}
-
-	return json.Unmarshal(bodyBytes, respPayload)
+func (a *API) SetAPIURL(url string) {
+	a.base.ApiURL = url
 }
---
+++ configuration/structures.go
@@ -0,0 +1,27 @@
+package configuration
+
+import "github.com/livechat/lc-sdk-go/configuration/action"
+
+type Webhook struct {
+	ID             string          `json:"webhook_id"`
+	Action         action.Webhook  `json:"action"`
+	SecretKey      string          `json:"secret_key"`
+	URL            string          `json:"url"`
+	AdditionalData []string        `json:"additional_data,omitempty"`
+	Description    string          `json:"description,omitempty"`
+	Filters        *WebhookFilters `json:"filters,omitempty"`
+}
+
+type WebhookFilters struct {
+	AuthorType    string `json:"author_type,omitempty"`
+	OnlyMyChats   bool   `json:"only_my_chats,omitempty"`
+	ChatMemberIds *struct {
+		AgentsAny     []string `json:"agents_any,omitempty"`
+		AgentsExclude []string `json:"agents_exclude,omitempty"`
+	} `json:"chat_member_ids,omitempty"`
+}
+
+type WebhookInfo struct {
+	Webhook
+	OwnerClientID string `json:"owner_client_id"`
+}
