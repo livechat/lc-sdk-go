79b7ab0dbac4f76e7dc095cbf20c030540c2286e
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2024-04-19 11:08:18 +0200

    check nil FormType before unmarshalling and add tests

--- webhooks/structures.go
+++ webhooks/structures.go
@@ -613,7 +613,7 @@ func (e *Event) FilledForm() *FilledForm {
 	if err := json.Unmarshal(e.Fields, &f.Fields); err != nil {
 		return nil
 	}
-	if err := json.Unmarshal(e.FormType, &f.FormType); err != nil && e.FormType != nil {
+	if err := internal.UnmarshalOptionalRawField(e.FormType, &f.FormType); err != nil {
 		return nil
 	}
 	return &f
