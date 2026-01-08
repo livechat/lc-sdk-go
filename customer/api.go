a023ca572c3b26b3597d79d22e6da5d05e2779a6
Marcin Dębski <debski.marcin.r@gmail.com>
2026-01-08 17:18:31 +0100

    [v3.6] add name_is_default (#223)
    Co-authored-by: Marcin Dębski <m.debski@livechat.com>

--- customer/api.go
+++ customer/api.go
@@ -265,9 +265,10 @@ func (a *API) DeleteEvent(chatID, threadID, eventID string) error {
 }
 
 // UpdateCustomer updates current customer's info.
-func (a *API) UpdateCustomer(name, email, avatarURL string, sessionFields []map[string]string) error {
+func (a *API) UpdateCustomer(name, email, avatarURL string, sessionFields []map[string]string, nameIsDefault *bool) error {
 	return a.Call("update_customer", &updateCustomerRequest{
 		Name:          name,
+		NameIsDefault: nameIsDefault,
 		Email:         email,
 		Avatar:        avatarURL,
 		SessionFields: sessionFields,
