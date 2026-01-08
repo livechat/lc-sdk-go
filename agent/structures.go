62a84b54f800c576358e6276f21ae1ffae003cf5
Marcin Dębski <m.debski@livechat.com>
2026-01-08 14:26:09 +0100

    [v3.6] add name_is_default

--- agent/structures.go
+++ agent/structures.go
@@ -308,6 +308,7 @@ type Agent struct {
 // Customer represents LiveChat customer.
 type Customer struct {
 	*User
+	NameIsDefault              bool                `json:"name_is_default"`
 	PhoneNumber                string              `json:"phone_number"`
 	Visit                      Visit               `json:"visit"`
 	Statistics                 CustomerStatistics  `json:"statistics"`
